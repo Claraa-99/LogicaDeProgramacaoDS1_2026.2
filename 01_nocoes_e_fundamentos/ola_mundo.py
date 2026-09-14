0758919ca178be9be27ade5bb1ce8bcfc378767a
Claraa-99 <maria.cruz441@aluno.ce.gov.br>
2026-09-14 13:28:52 -0300

    priemrio codigooo

--- 01_nocoes_e_fundamentos/ola_mundo.py
+++ 01_nocoes_e_fundamentos/ola_mundo.py
@@ -1,2 +1,13 @@
 print("Clara")
 print("Olá mundo!")
+
+
+nome = input("Digite seu nome:")
+dia = input("Digite o dia do Nascimento")
+mes = input("Digite o mês do Nascimento")
+ano = input("Digite o ano do Nascimento")
+
+print("Seu nome é", nome)
+print("O dia do seu nascimento é:", dia)
+print("O mês do seu nascimento é:", mes)
+print("Seu ano de nascimento é:", ano)
