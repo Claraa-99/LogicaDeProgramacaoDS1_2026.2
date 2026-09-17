8c47dd0726807bda5ecc5f34326a3c9229b68706
Claraa-99 <maria.cruz441@aluno.ce.gov.br>
2026-09-17 13:36:27 -0300

    exercico 05

--- 02_conceitos_basicos/exercicio_03_conta_sushi.py
+++ 02_conceitos_basicos/exercicio_03_conta_sushi.py
@@ -12,7 +12,7 @@ Crie um programa que:
 # TODO: Desenvolva o algoritmo abaixo:
 
 valor_consumido = float(input("Qual o valor consumido? "))
-taxa_servico = float(0.1)
-valor_final = (valor_consumido * taxa_servico) + taxa_servico
+taxa_servico = 0.1
+valor_final = valor_consumido * 0.1
 
-print(f"O valor a ser pago é de {valor_final:}!")
+print(f"O valor a ser pago é de {valor_final}!")
--- 02_conceitos_basicos/exercicio_05_media_ponderada.py
+++ 02_conceitos_basicos/exercicio_05_media_ponderada.py
@@ -9,3 +9,12 @@ Calcule e exiba a média final ponderada utilizando apenas operadores aritmétic
 """
 
 # TODO: Desenvolva o algoritmo abaixo:
+nota01 = float(input("Qual a nota 01? "))
+nota02 = float(input("Qual a nota 02? "))
+nota03 = float(input("Qual a nota 03? "))
+nota_1 = nota01 * 2
+nota_2 = nota02 * 3
+nota_3 = nota03 * 5
+media = (nota_3 + nota_2 + nota_1) / 3
+print(f"Olá, sua média é {media: .2f} ")
+
